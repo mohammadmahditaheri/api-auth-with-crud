73931790c9fdd11f8b6f4ee1b6b7cb705fd4d0fa
Mohammad Mahdi Taheri <mmtaheri@yandex.com>
2023-05-15 00:15:57 +0400

    + empty finalize controller + api routes for register and finalize register

---
+++ Modules/Auth/Http/Controllers/V1/FinalizeRegisterController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Modules\Auth\Http\Controllers\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class FinalizeRegisterController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
--- Modules/Auth/Routes/V1/api.php
+++ Modules/Auth/Routes/V1/api.php
@@ -2,7 +2,14 @@
 
 
 use Illuminate\Support\Facades\Route;
+use Modules\Auth\Http\Controllers\V1\FinalizeRegisterController;
+use Modules\Auth\Http\Controllers\V1\RegisterController;
 
-Route::get('/set', function () {
-    return 1;
-});
+// register first action
+Route::post('/register', RegisterController::class)
+    ->name('register')
+    ->middleware(['email_exists']);
+
+// register second action
+Route::post('/finalize-register', FinalizeRegisterController::class)
+    ->name('finalize.register');
