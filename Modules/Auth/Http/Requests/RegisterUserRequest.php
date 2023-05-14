f59766e2fe4dd0cafdd7881a2cafaab8d7bc38ff
Mohammad Mahdi Taheri <mmtaheri@yandex.com>
2023-05-15 00:10:34 +0400

    + form request for register route

---
+++ Modules/Auth/Http/Requests/RegisterUserRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Modules\Auth\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => [
+                'required',
+                'string',
+                'email',
+            ]
+        ];
+    }
+}
