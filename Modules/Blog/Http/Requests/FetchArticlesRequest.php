8c8ecb024ee97b2c897847bf98d8b87a6687bc98
Mohammad Mahdi Taheri <mmtaheri@yandex.com>
2023-05-16 01:42:03 +0400

    + fetch articles form request

---
+++ Modules/Blog/Http/Requests/FetchArticlesRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Modules\Blog\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class FetchArticlesRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'user_id' => 'integer',
+            'title' => 'string',
+            'body' => 'string',
+            'perPage' => 'integer'
+        ];
+    }
+}
