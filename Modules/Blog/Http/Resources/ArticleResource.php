0bbe6b58ea525a92eea0370bcc3444cc5587428b
Mohammad Mahdi Taheri <mmtaheri@yandex.com>
2023-05-16 01:42:18 +0400

    + api resource for article

---
+++ Modules/Blog/Http/Resources/ArticleResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Modules\Blog\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ArticleResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'user_id' => $this->user_id,
+            'title' => $this->title,
+            'body' => $this->body,
+            'created_at' => $this->created_at
+        ];
+    }
+}
