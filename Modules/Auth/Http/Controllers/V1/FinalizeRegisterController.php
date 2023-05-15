670397a0ca5539192bc068ce19c48fca78dbac60
Mohammad Mahdi Taheri <mmtaheri@yandex.com>
2023-05-15 22:06:44 +0400

    + finalize register invokable controller

--- Modules/Auth/Http/Controllers/V1/FinalizeRegisterController.php
+++ Modules/Auth/Http/Controllers/V1/FinalizeRegisterController.php
@@ -3,15 +3,51 @@
 namespace Modules\Auth\Http\Controllers\V1;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Response;
+use JetBrains\PhpStorm\ArrayShape;
+use Modules\Auth\Contracts\Repositories\UserRepositoryInterface;
+use Modules\Auth\Http\Requests\FinalizeRegisterRequest;
+use Modules\Auth\Traits\Responses\FormatsAuthResponses;
+use Modules\Auth\Traits\Responses\FormatsAuthErrorResponses;
 
 class FinalizeRegisterController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     */
-    public function __invoke(Request $request)
+    use FormatsAuthResponses,
+        FormatsAuthErrorResponses;
+
+    public function __construct(private UserRepositoryInterface $repository)
     {
-        //
+    }
+
+    /**
+     * Fulfill the registration process.
+     */
+    public function __invoke(FinalizeRegisterRequest $request): Response
+    {
+        $user = $this->repository->findByEmail($request->validated('email'));
+
+        // finalize the registration
+        $result = $this->repository->update(
+            $user,
+            $this->assembleFields($request)
+        );
+
+        return $result
+            ? $this->registeredSuccessfully()
+            : $this->registrationFailed();
+    }
+
+    /**
+     * assemble the updating fields for user
+     */
+    #[ArrayShape(['first_name' => "string", 'last_name' => "string", 'password' => "string"])]
+    private function assembleFields(FormRequest $request): array
+    {
+        return [
+            'first_name' => $request->validated('first_name'),
+            'last_name' => $request->validated('last_name'),
+            'password' => bcrypt($request->validated('password')),
+        ];
     }
 }
