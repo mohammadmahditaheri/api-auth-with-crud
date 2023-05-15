a13d099c1c6130d7901dcbef91da2a2c64d4fa1d
Mohammad Mahdi Taheri <mmtaheri@yandex.com>
2023-05-16 01:48:01 +0400

    + show own request

---
+++ Modules/Blog/Http/Requests/ShowOwnArticlesRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Modules\Blog\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ShowOwnArticlesRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'perPage' => 'integer'
+        ];
+    }
+}
