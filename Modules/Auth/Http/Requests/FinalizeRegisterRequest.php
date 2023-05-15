9ee92dd3f270844139f62a66b8b570b52547bf1c
Mohammad Mahdi Taheri <mmtaheri@yandex.com>
2023-05-15 22:05:41 +0400

    -+ zero-inform email existense validation

--- Modules/Auth/Http/Requests/FinalizeRegisterRequest.php
+++ Modules/Auth/Http/Requests/FinalizeRegisterRequest.php
@@ -2,10 +2,16 @@
 
 namespace Modules\Auth\Http\Requests;
 
+use Closure;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use Modules\Auth\Contracts\Repositories\UserRepositoryInterface;
+use Modules\Auth\Traits\Responses\FormatsAuthErrorResponses;
 
 class FinalizeRegisterRequest extends FormRequest
 {
+    use FormatsAuthErrorResponses;
+
     /**
      * Determine if the user is authorized to make this request.
      */
@@ -17,15 +23,15 @@ class FinalizeRegisterRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
+     * @param UserRepositoryInterface $repository
      * @return array
      */
-    public function rules(): array
+    public function rules(UserRepositoryInterface $repository): array
     {
         return [
-            'email' => [
+            'secret' => [
                 'required',
-                'string',
-                'email'
+                'string'
             ],
             'first_name' => [
                 'required',
@@ -42,7 +48,19 @@ class FinalizeRegisterRequest extends FormRequest
                 'string',
                 'min:8',
                 'confirmed'
-            ]
+            ],
+            'email' => [
+                'required',
+                'string',
+                'email',
+                // custom exists rule that does not inform user
+                // that the email does not exist
+                function (string $attribute, mixed $value, Closure $fail) use ($repository) {
+                    if (!$repository->emailExists($value)) {
+                        throw $this->finalizeRegistrationFailed();
+                    }
+                },
+            ],
         ];
     }
 }
