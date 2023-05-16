39498e379f34b526516685a7ba220fb5e55a605c
Mohammad Mahdi Taheri <mmtaheri@yandex.com>
2023-05-16 07:07:12 +0400

    + reset passowrd controller + forgot password method

--- Modules/Auth/Http/Controllers/V1/ResetPasswordController.php
+++ Modules/Auth/Http/Controllers/V1/ResetPasswordController.php
@@ -5,12 +5,34 @@ namespace Modules\Auth\Http\Controllers\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ForgotPasswordRequest;
 use App\Http\Requests\ResetPasswordRequest;
+use Illuminate\Http\Response;
+use Modules\Auth\Contracts\Actions\SendResetSecretHandlerInterface;
+use Modules\Auth\Contracts\Repositories\ResetPasswordRepoInterface;
+use Modules\Auth\Traits\Responses\FormatsResetResponses;
 
 class ResetPasswordController extends Controller
 {
-    public function forgotPassword(ForgotPasswordRequest $request)
+    use FormatsResetResponses;
+
+    public function __construct(
+        private SendResetSecretHandlerInterface $handler,
+        private ResetPasswordRepoInterface $repository
+    )
     {
-        //
+    }
+
+    /**
+     * manages sending secret code to user
+     *
+     * @param ForgotPasswordRequest $request
+     * @return Response
+     */
+    public function forgotPassword(ForgotPasswordRequest $request): Response
+    {
+        // send code by email via handler
+        $result = $this->handler->handle($request->validated('email'));
+
+        return $this->resetSecretSent();
     }
 
     public function resetPassword(ResetPasswordRequest $request)
